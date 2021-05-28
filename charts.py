82642a4cc7129b2778d5b5b64ecf0b4028a1b99f
Nikhil <nikhil.woodruff@outlook.com>
2021-05-28 11:30:53 +0100

    Add no-change bar

--- charts.py
+++ charts.py
@@ -6,12 +6,14 @@ def intra_decile_graph(baseline, reform_sim):
     COLORS = (
         "#9E9E9E",
         "#E0E0E0",
+        "#444444",
         "#C5E1A5",
         "#558B2F",
     )
     NAMES = (
         "Gain more than 5%",
         "Gain less than 5%",
+        "No change",
         "Lose less than 5%",
         "Lose more than 5%"
     )[::-1]
@@ -20,7 +22,7 @@ def intra_decile_graph(baseline, reform_sim):
     decile = income.decile_rank()
     gain = reform_sim.calc("household_net_income", map_to="person") - baseline.calc("household_net_income", map_to="person")
     rel_gain = (gain / baseline.calc("household_net_income", map_to="person")).dropna()
-    bands = (None, -0.05, 0.0, 0.05, None)
+    bands = (None, -0.05, -1e-2, 1e-2, 0.05, None)
     for lower, upper, color, name in zip(bands[:-1], bands[1:], COLORS, NAMES):
         fractions = []
         for i in range(1, 11):
