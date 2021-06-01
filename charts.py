925b1f88c6cf41c9de64c943e152b47e0d83ded8
Max Ghenis <mghenis@gmail.com>
2021-06-01 12:01:20 -0700

    Create within decile chart data function with multiple reforms

--- charts.py
+++ charts.py
@@ -1,15 +1,9 @@
 
 import plotly.graph_objects as go
+import pandas as pd
 
 
-def intra_decile_graph(baseline, reform_sim):
-    COLORS = (
-        "#9E9E9E",
-        "#E0E0E0",
-        "#444444",
-        "#C5E1A5",
-        "#558B2F",
-    )
+def intra_decile_graph_data(baseline, *reform_sims):
     NAMES = (
         "Gain more than 5%",
         "Gain less than 5%",
@@ -17,20 +11,35 @@ def intra_decile_graph(baseline, reform_sim):
         "Lose less than 5%",
         "Lose more than 5%"
     )[::-1]
+    l = []
+    for i, reform_sim in enumerate(reform_sims):
+        income = baseline.calc("equiv_household_net_income", map_to="person")
+        decile = income.decile_rank()
+        gain = reform_sim.calc("household_net_income", map_to="person") - baseline.calc("household_net_income", map_to="person")
+        rel_gain = (gain / baseline.calc("household_net_income", map_to="person")).dropna()
+        bands = (None, -0.05, -1e-3, 1e-3, 0.05, None)
+        for lower, upper, name in zip(bands[:-1], bands[1:], NAMES):
+            fractions = []
+            for i in range(1, 11):
+                subset = rel_gain[decile == i]
+                if lower is not None:
+                    subset = subset[rel_gain > lower]
+                if upper is not None:
+                    subset = subset[rel_gain <= upper]
+                fractions += [subset.count() / rel_gain[decile == i].count()]
+            tmp = pd.DataFrame({"reform": i, "fraction": fractions, "decile": list(range(1, 11)), "band": name})
+            l.append(tmp)
+        return pd.concat(l)
+
+
+def intra_decile_graph(base, reform):
+    COLORS = (
+        "#9E9E9E",
+        "#E0E0E0",
+        "#444444",
+        "#C5E1A5",
+        "#558B2F",
+    )
     fig = go.Figure()
-    income = baseline.calc("equiv_household_net_income", map_to="person")
-    decile = income.decile_rank()
-    gain = reform_sim.calc("household_net_income", map_to="person") - baseline.calc("household_net_income", map_to="person")
-    rel_gain = (gain / baseline.calc("household_net_income", map_to="person")).dropna()
-    bands = (None, -0.05, -1e-3, 1e-3, 0.05, None)
-    for lower, upper, color, name in zip(bands[:-1], bands[1:], COLORS, NAMES):
-        fractions = []
-        for i in range(1, 11):
-            subset = rel_gain[decile == i]
-            if lower is not None:
-                subset = subset[rel_gain > lower]
-            if upper is not None:
-                subset = subset[rel_gain <= upper]
-            fractions += [subset.count() / rel_gain[decile == i].count()]
-        fig.add_trace(go.Bar(x=fractions, y=list(range(1, 11)), name=name, orientation="h", marker_color=color))
-    return fig.update_layout(barmode="stack", title="Intra-decile outcomes", xaxis_title="Percentage of decile", xaxis_tickformat="%", yaxis_tickvals=list(range(1, 11)), yaxis_title="Decile")
+    fig.add_trace(go.Bar(x=fractions, y=list(range(1, 11)), name=name, orientation="h", marker_color=color))
+    fig.update_layout(barmode="stack", title="Intra-decile outcomes", xaxis_title="Percentage of decile", xaxis_tickformat="%", yaxis_tickvals=list(range(1, 11)), yaxis_title="Decile")
